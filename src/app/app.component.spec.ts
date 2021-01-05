7a361134384d420793d3ab8d190ea3b1cda848a2
charleswrepo <crwatson@live.co.uk>
2021-01-05 11:37:32 +0000

    Amending tslint.json to be less anal about trivial type annotations

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'my-portfolio'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-portfolio');
+    expect(app.pageTitle).toEqual('My Portfolio');
   });
 
   it('should render title', () => {

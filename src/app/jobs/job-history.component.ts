0863081c25fe0d01d4e4378aaf5389f3beb38816
charleswrepo <crwatson@live.co.uk>
2021-01-05 11:10:05 +0000

    Creating components

---
+++ src/app/jobs/job-history.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'mp-jobs',
+    templateUrl: './job-history.component.html',
+    styleUrls: ['./job-history.component.scss']
+})
+
+export class JobHistoryComponent {
+    pageTitle: string = 'Job History'
+}
---
+++ src/app/qualifications/qualification-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'mp-qualifications',
+    templateUrl: './qualification-list.component.html',
+    styleUrls: ['./qualification-list.component.scss']
+})
+
+export class QualificationListComponent {
+    pageTitle: string = 'Qualification List'
+}
